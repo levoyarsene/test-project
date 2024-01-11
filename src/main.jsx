f0f01b63e28b5865d3dad76c97977197af9173f0
levoyarsene <levoyarsene@gmai.com>
2024-01-11 11:51:57 +0700

    replace

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+const name = "naoifhosaofasho;fo";
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+  <h1>nigga {name}</h1>
+  <h2>nigg</h2>
+  </>
+)
+console.log('niggaaaa')
