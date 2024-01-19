e3f2da393ff529d69ca24bbdf1b0225c7078bb68
levoyarsene <levoyarsene@gmai.com>
2024-01-19 19:09:26 +0700

    add stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,18 @@
-
-import Header from "./components/header.jsx";
+import Footer from "./components/footer.jsx";
 import ProductItem from "./components/productItem.jsx";
-const productList = [
-    {
-      name: "Apple",
-      price: 100,
-      description: "This is a fruit 3",
-      image: "https://picsum.photos/200/300",
-    },
-    {
-      name: "Samsung",
-      price: 200,
-      description: "This is a fruit 1",
-      image: "https://picsum.photos/200/500",
-    }
-  ]
+import { useState } from "react";
+
 const App = () => {
-    return (
-        <div className="max-w-6xl mx-auto">
-            <Header />
-            <main>
-                <h2>Sản phẩm mới</h2>
-                <div className="grid grid-cols-3 gap-8">
-                   {productList.map((item) => {
-                    return <ProductItem product={item} key={item.id} />;
-                   })}
-                </div>
-            </main>
-            <Footer />
+    const [nigga] = useState([
+        {
+            
+        }
+    ])
+    return ( 
+        
+        <div>
+            <Footer></Footer>
         </div>
     );
 };
-
 export default App;
